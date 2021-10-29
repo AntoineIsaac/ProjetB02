fd68fb2b20023c20ff8a54ad2c1291b6bcbb4f07
Antoine <ant.olympe@live.be>
2021-10-29 08:55:48 +0200

    Start

--- ProjetB02/main.cpp
+++ ProjetB02/main.cpp
@@ -1,20 +1,18 @@
 #include <SFML/Graphics.hpp>
 int main()
 {
-sf::RenderWindow ventana(sf::VideoMode(800, 800), "Funciona!");
-sf::CircleShape circulo(200);
-circulo.setFillColor(sf::Color::Red);
-while (ventana.isOpen())
-{
-sf::Event event;
-while (ventana.pollEvent(event))
-{
-if (event.type == sf::Event::Closed)
-ventana.close();
-}
-ventana.clear();
-ventana.draw(circulo);
-ventana.display();
-}
-return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 800), "The running dead");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    return 0;
 }
